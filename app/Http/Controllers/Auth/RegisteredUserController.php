7a89d9820764ac6f5fdea93b0a6df7d04ba4d484
Luis Fernando <luisfernandocifuentes3@gmail.com>
2026-02-22 01:12:27 -0400

    auth: Agregada validacion de cedula y telefono en el registro

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -30,17 +30,21 @@ class RegisteredUserController extends Controller
     public function store(Request $request): RedirectResponse
     {
         $request->validate([
-            'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:'.User::class],
-            'password' => ['required', 'confirmed', Rules\Password::defaults()],
-        ]);
+        'name' => ['required', 'string', 'max:255'],
+        'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:'.User::class],
+        'document_id' => ['required', 'string', 'max:20', 'unique:'.User::class],
+        'phone' => ['required', 'string', 'max:20'],
+        'password' => ['required', 'confirmed', \Illuminate\Validation\Rules\Password::defaults()],
+]);
 
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
+            'document_id' => $request->document_id,
+            'phone' => $request->phone,
             'password' => Hash::make($request->password),
         ]);
-
+        $user->assignRole('Cliente');
         event(new Registered($user));
 
         Auth::login($user);
